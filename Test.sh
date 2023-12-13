0b423573aed4c758c0c8d4a5f2793dcf35291773
unknown <ponugotisudhakar468@gmail.com>
2023-12-13 11:26:29 +0530

    practiceTime

--- Test.sh
+++ Test.sh
@@ -4,7 +4,7 @@ NUM=$1
 
 if [ $NUM -gt 100 ]
 then
-echo "THIS is Moved to Next level"
+    echo "$NUM THIS is Greated than to Next level"
 else
-echo "$NUM is not Greater Than Value"
+    echo "$NUM is not Greater Than Value"
 fi
